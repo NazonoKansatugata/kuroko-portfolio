08f7573f9f73f22fc273773f9347d4ba75a3a9bf
NazonoKansatugata <0gy27b297263j6s@au.com>
2025-06-11 13:27:50 +0900

    画像変更

--- src/works.js
+++ src/works.js
@@ -3,31 +3,41 @@ const works = [
     title: 'Brakiocup2025',
     thumbnail: '/assets/video-a.png',
     description: 'READMEの構成を自動評価し、惑星の見た目やグラフで可視化するWebアプリ。Progate賞受賞。フロントエンド担当。React, three.js, chart.js, Vite, Go, Gemini API, GCP, Dockerなどを活用。',
-    url: 'https://github.com/Yanai1005/Brakiocup2025-front'
+    github: 'https://github.com/Yanai1005/Brakiocup2025-front',
+    youtube: 'https://youtu.be/nY8PTTqP7kc', // 実際のURLに置き換えてください
+    site: 'https://topaz.dev/projects/64b3740d8401a03d6ed7' // 実際のURLに置き換えてください
   },
   {
     title: 'きたキタ九州',
     thumbnail: '/assets/video-b.png',
     description: '北九州市の観光地やグルメ情報を提供するアプリ。dig-it-kitaqで学生が選ぶ最優秀賞受賞。Flutter, Firebase, Firestore, Google Maps APIを活用。',
-    url: 'https://github.com/DIGIT-KITAQ-Flutter/team-digfit'
+    github: 'https://github.com/DIGIT-KITAQ-Flutter/team-digfit',
+    youtube: 'https://youtu.be/bruLguCczE0', // 実際のURLに置き換えてください
+    site: '#' // 実際のURLに置き換えてください
   },
   {
     title: 'ぷろふぃーるはぶ',
     thumbnail: '/assets/video-c.png',
     description: 'オリキャラを制作し、トークを楽しめるアプリ。Flutter, Firebase, Gemini APIを活用。キャラクター制作・トーク画面・DB管理を担当。',
-    url: 'https://github.com/jyogi-web/geek2024_vol.22_1'
+    github: 'https://github.com/jyogi-web/geek2024_vol.22_1',
+    youtube: 'https://youtu.be/AAP77uR-0Fk', // 実際のURLに置き換えてください
+    site: '#' // 実際のURLに置き換えてください
   },
   {
     title: 'ゲームジャム作品',
     thumbnail: '/assets/video-d.png',
     description: 'DXライブラリで制作、猫が食材を集めて鍋を作る落ちものゲーム。C#を使用し、物理挙動や連鎖を実装。スコア更新やレベルアップ機能も搭載。',
-    url: '#'
+    github: '#',
+    youtube: 'https://youtu.be/ejDpz1PeHH8',
+    site: '#'
   },
   {
     title: '魔法少女ハル',
     thumbnail: '/assets/video-e.png',
     description: '魔法少女が人々の心を救うアクションゲーム。unityで制作、4つのミニゲームが一つになっている',
-    url: 'https://unityroom.com/games/masicgirl'
+    github: 'https://github.com/NazonoKansatugata/FSGJ_3_black', // 実際のURLに置き換えてください
+    youtube: '#', // 実際のURLに置き換えてください
+    site: 'https://unityroom.com/games/masicgirl'
   }
 ];
 
